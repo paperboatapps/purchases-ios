97aa7d28f8b49136f5d326faebeb1337a1d4e638
Kaushik Poria <kaushik@paperboatapps.com>
2020-07-16 20:27:07 +0530

    removed the ASIdentifierManager related code

--- Purchases/Purchasing/RCAttributionFetcher.m
+++ Purchases/Purchasing/RCAttributionFetcher.m
@@ -17,26 +17,10 @@
 
 @end
 
-@protocol FakeASIdentifierManager <NSObject>
-
-@property (nonnull, nonatomic, readonly) NSUUID *advertisingIdentifier;
-
-+ (instancetype)sharedManager;
-
-@end
-
 @implementation RCAttributionFetcher : NSObject
 
 - (nullable NSString *)advertisingIdentifier
 {
-    if (@available(iOS 6.0, macOS 10.14, *)) {
-        id<FakeASIdentifierManager> asIdentifierManagerClass = (id<FakeASIdentifierManager>)NSClassFromString(@"ASIdentifierManager");
-        if (asIdentifierManagerClass) {
-            return [asIdentifierManagerClass sharedManager].advertisingIdentifier.UUIDString;
-        } else {
-            RCDebugLog(@"AdSupport framework not imported. Attribution data incomplete.");
-        }
-    }
     return nil;
 }
 
